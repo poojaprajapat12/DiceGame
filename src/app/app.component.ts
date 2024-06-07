d4b80fafd9d5c98670fe1a178eb857c3037b97f7
Pooja <pooja.prajapat.sbg@gmail.com>
2024-06-07 23:29:58 +0530

    dice game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dice';
+   firstNumber:number=0;
+   secondNumber:number=0;
+   winner1!:string;
+  //  constructor(){
+  //   this.firstNumber=0;
+  //   this.secondNumber=0;
+  //  }
+   getNumber():void{
+     this.firstNumber = Math.floor(Math.random()*6)+1;
+     this.secondNumber = Math.floor(Math.random()*6)+1;
+     if(this.firstNumber>this.secondNumber)
+     {
+       console.log(this.winner1);
+       this.winner1 = "First Win!!"
+     }
+     else if(this.firstNumber<this.secondNumber){
+       this.winner1 = "Second Win!!"
+     }
+     else if(this.firstNumber==this.secondNumber)
+     {
+       this.winner1 =  "Draw!!"
+     }
+   }
+   resetNumbers(): void {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+  }
 }
